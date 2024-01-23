7783e8c75dd2df94834d56125abb8721707b8985
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2024-01-23 20:49:43 +0700

    add Input component

---
+++ components/ui/input.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from './input';
+
+const meta = {
+  title: 'Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => <Input type="email" placeholder="Email" />,
+};
