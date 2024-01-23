f3be0f68a9fca2cd0cf5fff058b20cc99bdb558a
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2024-01-23 16:47:01 +0700

    add-avatar-component

---
+++ components/ui/avatar.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Avatar, AvatarFallback, AvatarImage, AvatarProps } from './avatar';
+
+const sizeOptions: AvatarProps['size'][] = ['xs', 'sm', 'md', 'lg', 'xl'];
+const meta = {
+  title: 'Avatar',
+  component: Avatar,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    size: {
+      options: sizeOptions,
+    },
+  },
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+  args: {
+    size: 'md',
+  },
+
+  render: (args) => (
+    <Avatar size={args.size}>
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  ),
+};
