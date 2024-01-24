616ee77a728825b2056b2506cdbc23fee9138e74
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2024-01-24 23:20:18 +0700

    add Toast component

---
+++ components/ui/toast.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Toaster, toast } from './toast';
+import { Button } from '.';
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof Toaster> = {
+  title: 'Toaster',
+  component: Toaster,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Toaster>;
+
+export const Default: Story = {
+  render: (args) => {
+    return (
+      <>
+        <Toaster />
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast('Event has been created', {
+              description: 'Sunday, December 03, 2023 at 9:00 AM',
+              action: {
+                label: 'Undo',
+                onClick: () => {
+                  //  console.log('Undo');
+                },
+              },
+            })
+          }
+        >
+          Show Toast
+        </Button>
+      </>
+    );
+  },
+};
